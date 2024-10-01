0e9d8aba0d00442df2f5d6d6d6cc2a920c7edb44
Colin de Vries <colindv2000@gmail.com>
2024-10-01 22:56:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     long_description_content_type="text/markdown",
-    long_description=open("README.md").read(),  # pylint: disable=unspecified-encoding
+    long_description=open("README.md").read(),  # pylint: disable=unspecified-encoding, consider-using-with
 )
